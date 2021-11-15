b0efa6d68677f18835ed77f3574926442088c7fe
gimunaretto <gimunaretto@gmail.com>
2021-11-15 10:26:36 -0300

    Add Call CarrinhoItem

---
+++ LetsShop/Model/CarrinhoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsShop.Model
+{
+    public class CarrinhoItem
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+        public int Total { get; set; }
+    }
+}
